f9df1847f1f2c11f9bd61b4977e0e395f32084a3
Greg Book <gregory.a.book@gmail.com>
2017-03-07 17:26:48 -0500

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -31,25 +31,19 @@ void MainWindow::on_pushButton_clicked()
 /* -------------------------------------------------------------------- */
 /* ------------ GenerateColorGradient --------------------------------- */
 /* -------------------------------------------------------------------- */
-QString GenerateColorGradient(unsigned char R1, unsigned char G1, unsigned char B1, unsigned char R2, unsigned char G2, unsigned char B2, int gradientSize, unsigned char colorR[], unsigned char colorG[], unsigned char colorB[]) {
+QString MainWindow::GenerateColorGradient(unsigned char R1, unsigned char G1, unsigned char B1, unsigned char R2, unsigned char G2, unsigned char B2, int gradientSize, unsigned char colorR[], unsigned char colorG[], unsigned char colorB[]) {
 
     QString str;
     /* generate gradient of 0 - 255 */
-    for (int i=0; i<100; i++) {
-        double percentSR = ((double)i/(double)gradientSize)*(double)R1;
-        double percentER = (1.0-((double)i/(double)gradientSize))*(double)R2;
-        colorR[i] = (int)percentSR + (int)percentER;
+    for (int i=0; i<gradientSize; i++) {
+        double ratio = (double)i/(double)gradientSize;
 
-        double percentSG = ((double)i/(double)gradientSize)*(double)G1;
-        double percentEG = (1.0-((double)i/(double)gradientSize))*(double)G2;
-        colorG[i] = (int)percentSG + (int)percentEG;
-
-        double percentSB = ((double)i/(double)gradientSize)*(double)B1;
-        double percentEB = (1.0-((double)i/(double)gradientSize))*(double)B2;
-        colorB[i] = (int)percentSB + (int)percentEB;
+        colorR[i] = (int)(((double)R2-(double)R1)*ratio)+R1;
+        colorG[i] = (int)(((double)G2-(double)G1)*ratio)+G1;
+        colorB[i] = (int)(((double)B2-(double)B1)*ratio)+B1;
 
         char buffer[50];
-        int nChar = sprintf(buffer, "%d, %d, %d, 255", colorR[i], colorG[i], colorB[i]);
+        int nChar = sprintf(buffer, "%d) %d, %d, %d\n", i, colorR[i], colorG[i], colorB[i]);
         str += buffer;
     }
     return str;
